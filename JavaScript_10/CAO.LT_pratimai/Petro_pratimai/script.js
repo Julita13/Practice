0c878a3a173ebfa9f5e42c6e2727a4f4bc92af15
Julita13 <julitamazuolyte@yahoo.com>
2022-10-24 23:11:54 +0300

    minor changes added

--- JavaScript_10/CAO.LT_pratimai/Petro_pratimai/script.js
+++ JavaScript_10/CAO.LT_pratimai/Petro_pratimai/script.js
@@ -13,3 +13,21 @@ console.log(person.cars);
 console.log(person.cars[0]);
 console.log(person.cars[0].params);
 console.log(person.cars[0].params[0]);
+
+
+console.log(document.location);
+
+if (document.location.search === "?color=red") {
+    document.body.style.background = "green";
+};
+
+document.querySelector("button").addEventListener("click", () => {
+    document.location.replace("http://google.com");
+})
+
+//  tik tuo atveju, jei nurodai: http://127.0.0.1:5500/JavaScript_10/CAO.LT_pratimai/Petro_pratimai/index.html?color=red
+
+//  Document Location valdymas:
+// Nuėjus į Delfi > naujausi > straipsnis --- kaip grįžti į pradinį puslapį? 
+// ogi pasinaudojus: 
+// document.location.replace("/")
