d61fd31acb71037be7676ddd47e693fce7e00c0b
Julita13 <julitamazuolyte@yahoo.com>
2022-11-09 22:29:23 +0200

    tasks completed

--- JavaScript_16/CAO.LT_pratimai/Kas_tas_OOP/main.js
+++ JavaScript_16/CAO.LT_pratimai/Kas_tas_OOP/main.js
@@ -26,34 +26,51 @@ function Car(b, m, e) {
   }
   
   const myCar = new Car("Chrysler", "Sebring", "2.0 l");
-  console.log(myCar.brand);
-  
   const mySecondCar = new Car("Toyota", "Aygo", "1.0 l");
-  console.log(mySecondCar.model)
+  console.log(myCar.brand);
+  console.log(mySecondCar.model);
+  myCar.turnOn();
+
+// ANTRAS BŪDAS: SU CLASS + CONSTRUCTOR:
+
+// class Car {
+//   constructor(brand, model, engine) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.engine = engine;
+//     this.turnOn = function (){
+//       alert ("vrooom");
+//     }
+//   }
+// }
 
 //   2.Pratimas
 // Pakoreguokite šį konstruktorių ir pridėkite papildomą property 'basePrice' ir metodą 'getPrice'. 
 // basePrice propertį įrašys sukuriant objektą, tačiau getPric priklausimai nuo variklio išmes kokia yra galutinė kaina. 
 // Jei variklis 'electric' - kaina bus +10,000; jei 'diesel' +5,000; jei 'petrol' - kaina tokia kokia ir basePrice.
 
-function Automobilis(brand, model, engine, price) {
-    this.brand = brand;
-    this.model = model;
-    this.engine = engine;
-    this.basePrice = price;
-    this.getPrice = function() {
-      let finalPrice = 0; 
-      if(this.engine === "electric") {
-        finalPrice = 10000;
-      } else if(this.engine === "diesel") {
-         finalPrice = 5000;
-      }
-      return this.basePrice + finalPrice;
-      }
-    this.turnOn = function () {
-      alert("vrooom")
-    };
+function Automobilis(b, m, e, p) {
+  this.brand = b;
+  this.model = m;
+  this.engine = e;
+  this.basePrice = p;
+  this.turnOn = function() {
+    alert("vrooom")
   }
-  
-  const newCar = new Automobilis("Chrysler", "Sebring", "petrol", 5000);
-  console.log(newCar.getPrice());
+  this.getPrice = function() {
+    let basePrice = this.basePrice;
+    if(this.engine === "electric") {
+      return basePrice + 10000;
+    } else if(this.engine === "diesel") {
+      return basePrice + 5000;
+    } 
+    return basePrice;
+  }
+}
+
+const firstCar = new Automobilis("Chrysler", "Sebring", "diesel", 2000);
+const secondCar = new Automobilis("Toyota", "Aygo", "electric", 8000);
+console.log(firstCar.basePrice);
+console.log(secondCar.model);
+console.log(firstCar.getPrice());
+console.log(secondCar.getPrice());
