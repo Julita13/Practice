95ad399e0319fa066fabe64fde9f5202d2e30e1c
Julita13 <julitamazuolyte@yahoo.com>
2022-11-19 21:47:06 +0200

    little bit of theory added

---
+++ JavaScript_21/CAO.LT_pratimai/main.js
@@ -0,0 +1,20 @@
+// API - APPLICATION PROGRAMMING INTERFACE - basically it is something,
+// that allows one peace of software to talk to another. Front + back.
+
+// REST - REPRESENTATIONAL STATE TRANSFER
+
+// https://graph.facebook.com/youtube?fields=id,name,likes
+
+// http://maps.googleapis.com/maps/api/geocode/json
+
+// maps.googleapis.com / maps / api / geocode / json ? address = chicago & sensor=false
+
+// METHODS: 
+
+// GET AND POST
+
+// PAGRINDINIS FETCH: 
+// fetch(url)
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+//   .catch(e => console.log(e.message))
