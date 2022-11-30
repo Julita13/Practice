c39371c3c26d77f5ca9e99b7dd442b59f32d8801
Julita13 <julitamazuolyte@yahoo.com>
2022-11-30 20:25:49 +0200

    daily task added

--- JavaScript_25/Practice/main.js
+++ JavaScript_25/Practice/main.js
@@ -1,39 +1,74 @@
-import { fetchData, postData } from "./src/crud.js";
-function displayData(tasks){
+import { deleteData, fetchData, postData, updateData } from "./src/crud.js";
+const descriptionInput = document.querySelector("#description");
+const dateInput = document.querySelector("#date");
+const textInput = document.querySelector("#task");
+let editMode = false;
+
+function displayData(tasks) {
     const taskContainer = document.querySelector(".tasks");
     let taskDiv = "";
     tasks.forEach(task => {
-        taskDiv += `<div>
+        taskDiv += `<div id="${task.id}" class="task_div">
         <h2>${task.text}</h2>
         <h3>${task.date}</h3>
         <p>${task.description}</p>
         <span class="options">
-            <i class="fas fa-edit"></i>
+            <i data-bs-toggle="modal" data-bs-target="#exampleModal" class="fas fa-edit"></i>
             <i class="fas fa-trash-alt"></i>
             </span>
             </div>`
-        });
-        taskContainer.innerHTML = taskDiv;
-    }
-    const formEl = document.querySelector("form");
-    formEl.onsubmit = async (event) => {
-        event.preventDefault();
-        const textInput = document.querySelector("#task").value;
-        if(textInput === ""){
-            const errorDiv = document.querySelector(".error");
-            errorDiv.textContent = "Privalomas laukas";   
-        }else{
-            const descriptionInput = document.querySelector("#description").value;
-            const dateInput = document.querySelector("#date").value;
+    });
+    taskContainer.innerHTML = taskDiv;
+
+    const deleteIcons = document.querySelectorAll(".fa-trash-alt");
+    deleteIcons.forEach(icon => {
+        icon.onclick = (event) => {
+            event.preventDefault();
+            const id = event.target.parentElement.parentElement.id;
+            deleteData(id).then(item => displayData(item))
+        }
+    })
+    const updateIcons = document.querySelectorAll(".fa-edit");
+    updateIcons.forEach(icon => {
+        icon.onclick = (event) => {
+            event.preventDefault();
+            editMode = true
+            const task = event.target.parentElement.parentElement;
+            textInput.value = task.children[0].textContent;
+            dateInput.value = task.children[1].textContent;
+            description.value = task.children[2].textContent;
+            const modalId = document.querySelector(".modal-title");
+            modalId.innerHTML = `Redaguoti <span>${task.id}</span>`
+        }
+    })
+
+}
+const formEl = document.querySelector("form");
+formEl.onsubmit = async (event) => {
+    event.preventDefault();
+    if (textInput.value === "") {
+        const errorDiv = document.querySelector(".error");
+        errorDiv.textContent = "Privalomas laukas";
+    } else {
+        if(!editMode){
             const data = {
-                text: textInput,
-                date: dateInput,
-                description: descriptionInput,
-                id: Math.floor(Math.random()*1000 + 1),
+                text: textInput.value,
+                date: dateInput.value,
+                description: descriptionInput.value,
+                id: Math.floor(Math.random() * 1000 + 1),
             }
             postData(data).then(item => displayData(item))
+        } else {
+            editMode = false;
+            const id = document.querySelector(".modal-title > span").textContent; 
+            const updatedData = {
+                text: textInput.value,
+                date: dateInput.value,
+                description: descriptionInput.value,
+                id: id,
+            }
+            updateData(updatedData).then(item=>displayData(item))
         }
     }
-
-
-    (()=>{fetchData().then(data => displayData(data))})();
+}
+(() => { fetchData().then(data => displayData(data)) })();
--- JavaScript_25/Practice/src/crud.js
+++ JavaScript_25/Practice/src/crud.js
@@ -27,3 +27,32 @@ export const fetchData = async () => {
         console.log(error)
     }
   }
+  export const deleteData = async (id) => {
+    try{
+        const response = await fetch(dataApi+"/data/"+id, {
+            method: "DELETE",   
+        });
+        if(response.ok){
+            return await response.json()
+        }
+    }catch (error) {
+        console.log(error)
+    }
+  }
+  export const updateData = async (data) => {
+    try{
+        const response = await fetch(dataApi+"/data/"+data.id, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                Accept: "application/json",
+                'Content-Type': 'application/json',
+            }
+        });
+        if(response.ok){
+            return await response.json()
+        }
+    }catch (error) {
+        console.log(error)
+    }
+  }
