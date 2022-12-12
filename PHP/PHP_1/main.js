afd857dcc7e3a2ff2241c446b37aedc1d36e05f6
Julita13 <julitamazuolyte@yahoo.com>
2022-12-12 23:26:19 +0200

    daily php info added

---
+++ PHP/PHP_1/main.js
@@ -0,0 +1,37 @@
+//  PHP - Personal Home Page
+// Jį naudoja: FB, Wikipedia,Tumblr, Slack, MailChimp, Etsy, WordPress ... Tai - interpretuojama kalba. Arba - skriptinė kalba - kalba, parašyta 
+// kita programavimo kalba. 
+// cd php8.2 = cd ./php8.2 < --- tas pats užrašymas
+
+// norint nueiti terminale į C drive'ą, reikia spausti cd /c
+
+// PS C:\server\php8.2> php.exe ../hello2.php
+//   powershelle rašoma: PS C:\server\php8.2> .\php.exe ../hello2.php
+
+// VS Code rašyti reiktų: ./php.exe ../hello2.php
+
+//  Norint išgauti tekstą, reikia pačiame php faile parašyti > ?php
+
+//  Shelle sukurti naują failą: PS C:\server\php8.2> New-Item labas.txt
+
+
+//  printas priima vieną, echo priima kelis
+
+//  php nepaiso tarpų
+
+
+//  PHP failas: 
+
+// <?php
+// // print "5+5+5 ";
+// echo "Buenos dias ", "Labas ", "Bonjour ";
+// echo "\n\n"; // prideda naują eilutę
+// echo "Mano vardas Julita ";
+// echo "\n\n";
+
+// $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+// Commodi est officiis consequatur repellat officia impedit neque! 
+// Doloribus velit inventore aperiam laudantium voluptates veniam 
+// minima eveniet provident vero! Iusto, veritatis eum.";
+// echo $text;
+// ?>
