d16b6cf1d116eba75935d96ca1d4b9d6413bd7f0
Julita13 <julitamazuolyte@yahoo.com>
2023-02-27 23:12:25 +0200

    last bug fixed

--- PHP/PHP_39/MySQL/movie_rentals/language_movie/1_create_language_movie_table.sql
+++ PHP/PHP_39/MySQL/movie_rentals/language_movie/1_create_language_movie_table.sql
@@ -2,12 +2,11 @@
 DROP TABLE IF EXISTS `movie_rentals`.`language_movie`;
 CREATE TABLE IF NOT EXISTS `movie_rentals`.`language_movie`(
     `language_id` INT UNSIGNED NOT NULL,
-    `movie_id` INT UNSIGNED NOT NULL
+    `movie_id` INT UNSIGNED NOT NULL,
 
-    -- CONSTRAINT `fk_language_movie_languages` FOREIGN KEY(`language_id`) REFERENCES `languages`(`id`) ON UPDATE CASCADE ON DELETE RESTRICT,
-    -- CONSTRAINT `fk_language_movie_movies` FOREIGN KEY(`movie_id`) REFERENCES `movies`(`id`) ON UPDATE CASCADE ON DELETE RESTRICT
-
-    -- UNIQUE CONSTAINT: 
-    -- kurių stulpelių vertės bus unikalios
-    -- CONSTRAINT `uc_language_id_movie_id` UNIQUE KEY (`language_id`, `movie_id`)
+    CONSTRAINT `fk_language_movie_languages` FOREIGN KEY (`language_id`) REFERENCES `languages`(`id`) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT `fk_language_movie_movies` FOREIGN KEY (`movie_id`) REFERENCES `movies`(`id`) ON UPDATE CASCADE ON DELETE RESTRICT,
+    -- UNIQUE CONSTRAINT
+    -- kurių stulpeliu vertės bus unikalios
+    CONSTRAINT `uc_language_id_movie_id` UNIQUE KEY (`language_id`,`movie_id`)
 );
