c2f70b831f5a738770d6c363f9f087e2a5b83516
Julita13 <julitamazuolyte@yahoo.com>
2023-02-27 23:03:35 +0200

    small minor fixes added

--- PHP/PHP_39/MySQL/movie_rentals/genres/1_create_genres_table.sql
+++ PHP/PHP_39/MySQL/movie_rentals/genres/1_create_genres_table.sql
@@ -1,5 +1,6 @@
 -- Active: 1676911955202@@127.0.0.1@3306@movie_rentals
 
+DROP TABLE IF EXISTS `movie_rentals`.`genres`;
 CREATE TABLE
     IF NOT EXISTS `movie_rentals`.`genres` (
         `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
