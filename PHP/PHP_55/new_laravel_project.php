885eda2028eadc71bac99689b8a2f0d73a5820ab
Julita13 <julitamazuolyte@yahoo.com>
2023-03-27 18:31:41 +0300

    new project created

---
+++ PHP/PHP_55/new_laravel_project.php
@@ -0,0 +1,11 @@
+<?php
+
+// composer create-project laravel/laravel example-app
+//  composer create-project laravel/laravel movie_rentals
+// httpd -k restart
+// Po to, nuėjus į http://localhost/Laravel_Project/laravel movie_rentals/public/index.php turi matytis laravelio dokumentacija
+
+
+
+// composer create-project laravel/laravel travels
+?>
