e9732ec1dcb8d6af8d551fddd9f4275c0cf11215
Julita13 <julitamazuolyte@yahoo.com>
2023-04-01 00:37:59 +0300

    welcome page created

--- PHP/Baigiamasis/travels/resources/views/admin/home.blade.php
+++ PHP/Baigiamasis/travels/resources/views/admin/home.blade.php
@@ -1,2 +1,7 @@
-Welcome to travel's project!
+<audio src="{{asset('storage/music/travels2.mp3')}}" loop autoplay ></audio>
+{{-- <audio src="{{asset('storage/music/travels.mp3')}}" loop autoplay ></audio> --}}
+<img  style="display: block; margin-left: auto; margin-right: auto; width: 50%; " src="https://travelthe.world/wp-content/uploads/2019/02/travel-the-world.png" alt="">
+<button style="display: block; margin-left: auto; margin-right: auto; width: 5%; border-radius: 0.5rem; padding: 0.3rem; background: lightblue; border-color: rgb(173, 173, 218)">
+    <a href="{{route('admin.destinations.index')}}" style="color:rgb(86, 86, 156); text-decoration:none">Press me</a>
+</button>
 
