1a7838f24050f60a8781977a8625640ee1cb21f1
Julita13 <julitamazuolyte@yahoo.com>
2023-04-03 18:43:54 +0300

    minor bug fixes added

--- PHP/Baigiamasis/travels/resources/views/admin/home.blade.php
+++ PHP/Baigiamasis/travels/resources/views/admin/home.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ asset('css/admin/main.css') }}">
 
 <div>
-    <audio src="{{asset('storage/music/travels2.mp3')}}" loop autoplay ></audio>
+    <audio src="{{asset('storage/music/travels2.mp3')}}" loop autoplay  ></audio>
     <img  class="travels" src="https://travelthe.world/wp-content/uploads/2019/02/travel-the-world.png" alt="">
 
     <div class="container">
